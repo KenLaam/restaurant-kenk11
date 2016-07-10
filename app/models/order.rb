d38fb0f7104970b89ad77760963b5f7edd02b37e
kenk11 <hoangvu23108@gmail.com>
2016-07-11 02:00:36 +0700

    update time format

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :food_item
   validates :food_item, :name, :phone, :address, presence: true
+
+  def time
+    self[:created_at].strftime('%A, %B %d, %r')
+  end
 end
